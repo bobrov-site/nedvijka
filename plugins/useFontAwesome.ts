e373f5886ce9bdc966cc30217a1a213f5b79a286
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-05 14:03:17 +0300

    add new icons

--- plugins/useFontAwesome.ts
+++ plugins/useFontAwesome.ts
@@ -1,8 +1,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMessage, faUser } from '@fortawesome/free-regular-svg-icons';
 import { faHome } from '@fortawesome/free-solid-svg-icons'; // Import the home icon
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  library.add(faHome); // Add the home icon to the library
+  library.add(faHome, faMessage, faUser); // Add the home icon to the library
   nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon);
 });
