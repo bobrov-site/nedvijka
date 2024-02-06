5df2c32e97cef8ab9b55f48c6f7341ce99778e06
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-06 14:43:07 +0300

    added todo

--- components/header/Profile.vue
+++ components/header/Profile.vue
@@ -18,23 +18,24 @@
                     <h4>Не зарегистрированы?</h4>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleDropdownFormEmail1" class="form-label">Адрес электронной
+                    <label for="email" class="form-label">Адрес электронной
                         почты</label>
-                    <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
+                    <input type="email" class="form-control" id="email">
                 </div>
                 <div class="mb-3">
-                    <label for="exampleDropdownFormPassword1" class="form-label">Пароль</label>
-                    <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+                    <label for="password" class="form-label">Пароль</label>
+                    <input type="password" class="form-control" id="password">
                 </div>
                 <div class="mb-3">
-                    <label for="exampleDropdownFormPassword1" class="form-label">Подтвердить
+                    <label for="passwordConfirm" class="form-label">Подтвердить
                         пароль</label>
-                    <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+                    <input type="password" class="form-control" id="passwordConfirm">
                 </div>
                 <div class="mb-3">
                     <div class="form-check">
                         <input type="checkbox" class="form-check-input" id="dropdownCheck">
                         <label class="form-check-label" for="dropdownCheck">
+                            <!-- TODO ссылка на политику -->
                             Ознакомился с политикой конфединциальности
                         </label>
                     </div>
