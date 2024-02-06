52ace6c31460d1366d99053c7a5325755c8cb6f3
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-06 17:12:02 +0300

    added calendar icon

--- plugins/useFontAwesome.ts
+++ plugins/useFontAwesome.ts
@@ -1,10 +1,10 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faWhatsapp, faTelegram, faViber } from '@fortawesome/free-brands-svg-icons';
-import { faMessage, faUser } from '@fortawesome/free-regular-svg-icons';
+import { faMessage, faUser, faCalendar } from '@fortawesome/free-regular-svg-icons';
 import { faHome } from '@fortawesome/free-solid-svg-icons'; // Import the home icon
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  library.add(faHome, faMessage, faUser, faWhatsapp, faTelegram, faViber); // Add the home icon to the library
+  library.add(faHome, faMessage, faUser, faWhatsapp, faTelegram, faViber, faCalendar); // Add the home icon to the library
   nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon);
 });
