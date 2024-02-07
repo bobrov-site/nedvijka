b240c5ef13b10a55f0881566d294878e3f0580c2
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-07 11:51:05 +0300

    created util parseDate

---
+++ utils/parseDate.js
@@ -0,0 +1,6 @@
+export default function formatDate(date) {
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+}
