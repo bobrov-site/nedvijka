edb6429e34b1daa388c092dd09abb5c986a94d8c
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-07 13:35:56 +0300

    created SliderBenefits

---
+++ components/SliderBenefits.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="card">
+                    <NuxtImg src="/images/support.jpg" alt="Круглосуточная поддержка" class="card-img-top" sizes="md:300px"/>
+                    <!-- <img src="~/assets/img/support.png" class="card-img-top" alt="Круглосуточная поддержка"> -->
+                    <div class="card-body">
+                        <h4 class="card-title">Преимущества сервиса</h4>
+                        <p class="card-text">
+                            Круглосуточная поддержка
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="...">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
