4fd5769f9a90c2ecee52ce25a63d810e118f86f1
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-07 14:03:58 +0300

    added title for search form

--- components/SearchForm.vue
+++ components/SearchForm.vue
@@ -12,8 +12,13 @@ const handleDate = (modelData) => {
 
 <template>
     <div class="card p-4 search-form">
+        <div class="row mb-3">
+            <div class="col-12">
+                <h2>Заголовок формы (возможно лозунг)</h2>
+            </div>
+        </div>
         <form class="row g-3">
-            <div class="col-md-3">
+            <div class="col-md-6">
                 <label for="datalist" class="form-label">Выберите город</label>
                 <input class="form-control" list="datalistOptions" id="datalist" placeholder="Ставрополь">
                 <datalist id="datalistOptions">
@@ -22,32 +27,7 @@ const handleDate = (modelData) => {
                     <option value="Кисловодск" />
                 </datalist>
             </div>
-            <div class="col-md-3">
-                <label for="start" class="form-label">Заезд</label>
-                <VueDatePicker v-model="date" @update:model-value="handleDate" :auto-position="false" auto-apply
-                    id="start" range text-input :enable-time-picker="false" :clearable="false" locale="ru">
-
-                    <template #dp-input>
-                        <input type="text" :value="start" class="form-control">
-                    </template>
-                    <template #input-icon>
-                        <FontAwesomeIcon v-if="!start" class="datepicker-calendar-icon" :icon="['far', 'calendar']" />
-                    </template>
-                </VueDatePicker>
-            </div>
-            <div class="col-md-3">
-                <label for="end" class="form-label">Выезд</label>
-                <VueDatePicker v-model="date" @update:model-value="handleDate" :auto-position="false" auto-apply
-                    id="end" range text-input :enable-time-picker="false" locale="ru" :clearable="false">
-                    <template #dp-input>
-                        <input type="text" :value="end" class="form-control">
-                    </template>
-                    <template #input-icon>
-                        <FontAwesomeIcon v-if="!end" class="datepicker-calendar-icon" :icon="['far', 'calendar']" />
-                    </template>
-                </VueDatePicker>
-            </div>
-            <div class="col-md-3">
+            <div class="col-md-6">
                 <label for="guests" class="form-label">Гости</label>
                 <button type="button" class="form-control dropdown-toggle text-end" id="guests" data-bs-toggle="dropdown"
                     data-bs-auto-close="false" aria-expanded="false"></button>
@@ -91,8 +71,33 @@ const handleDate = (modelData) => {
                     </li>
                 </ul>
             </div>
+            <div class="col-md-6">
+                <label for="start" class="form-label">Заезд</label>
+                <VueDatePicker v-model="date" @update:model-value="handleDate" :auto-position="false" auto-apply
+                    id="start" range text-input :enable-time-picker="false" :clearable="false" locale="ru">
+
+                    <template #dp-input>
+                        <input type="text" :value="start" class="form-control">
+                    </template>
+                    <template #input-icon>
+                        <FontAwesomeIcon v-if="!start" class="datepicker-calendar-icon" :icon="['far', 'calendar']" />
+                    </template>
+                </VueDatePicker>
+            </div>
+            <div class="col-md-6">
+                <label for="end" class="form-label">Выезд</label>
+                <VueDatePicker v-model="date" @update:model-value="handleDate" :auto-position="false" auto-apply
+                    id="end" range text-input :enable-time-picker="false" locale="ru" :clearable="false">
+                    <template #dp-input>
+                        <input type="text" :value="end" class="form-control">
+                    </template>
+                    <template #input-icon>
+                        <FontAwesomeIcon v-if="!end" class="datepicker-calendar-icon" :icon="['far', 'calendar']" />
+                    </template>
+                </VueDatePicker>
+            </div>
             <div class="col-12">
-                <button type="submit" class="btn btn-primary w-100">Поиск</button>
+                <button type="submit" class="btn btn-lg btn-primary w-100">Поиск</button>
             </div>
         </form>
     </div>
