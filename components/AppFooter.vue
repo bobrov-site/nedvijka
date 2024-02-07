6b2018f281638600b4c4e8833d7c76674e5e6da7
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-07 16:17:13 +0300

    added layout for footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,6 +1,13 @@
 <template>
-    <footer>
-        
+    <footer class="py-3 mt-4 bg-secondary">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">About</a></li>
+        </ul>
+        <p class="text-center text-light">© 2023 Company, Inc</p>
     </footer>
 </template>
 
@@ -8,6 +15,4 @@
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
