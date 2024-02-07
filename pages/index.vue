59c1cc7f390cac542032d53750bf3b550016f11a
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-07 16:17:49 +0300

    imported CardApartment in index.vue page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,20 @@
 <template>
-    <div class="row">
+    <div class="row mt-4">
         <div class="col-12 col-md-8">
             <SearchForm/>
         </div>
         <div class="col-12 col-md-4">
             <SliderBenefits/>
+        </div>
+    </div>
+    <div class="row mt-4 mb-2">
+        <div class="col-12">
+            <h2>Доступные квартиры</h2>
+        </div>
+    </div>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+        <div class="col">
+            <CardApartment/>
         </div>
     </div>
 </template>
