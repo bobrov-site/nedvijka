5194db10498fd92a55aaaac9a1600afe86dc8a38
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-08 13:32:33 +0300

    update footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,11 +1,9 @@
 <template>
-    <footer class="py-3 mt-4 bg-secondary">
+    <footer class="py-3 mt-4 bg-primary">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">About</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Главная</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Поддержка</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Личный кабинет</a></li>
         </ul>
         <p class="text-center text-light">© 2023 Company, Inc</p>
     </footer>
