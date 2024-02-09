d772b8621c3a66a1f2e160414c31e06ffa02fc1a
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-09 11:45:08 +0300

    imported all fontAwesome icons

--- plugins/useFontAwesome.ts
+++ plugins/useFontAwesome.ts
@@ -1,10 +1,10 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faWhatsapp, faTelegram, faViber } from '@fortawesome/free-brands-svg-icons';
-import { faMessage, faUser, faCalendar } from '@fortawesome/free-regular-svg-icons';
-import { faHome } from '@fortawesome/free-solid-svg-icons'; // Import the home icon
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons'; // Import the home icon
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  library.add(faHome, faMessage, faUser, faWhatsapp, faTelegram, faViber, faCalendar); // Add the home icon to the library
+  library.add(fas, far, fab); // Add the home icon to the library
   nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon);
 });
