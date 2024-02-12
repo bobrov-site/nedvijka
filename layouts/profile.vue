582546ededbfbac12f741be43c7f297813b05463
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-12 13:54:11 +0300

    change name of layout from profile to user

--- layouts/profile.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="app">
-        <AppHeader />
-        <div class="container mb-4 mt-4">
-            <slot />
-        </div>
-        <AppFooter />
-    </div>
-</template>
