9d010db876d164f2ba98547468932c0f8264d353
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-12 14:25:35 +0300

    added registration and login page

--- components/header/Profile.vue
+++ components/header/Profile.vue
@@ -1,53 +1,25 @@
+<script setup>
+const user = useUserStore();
+</script>
 <template>
     <div class="dropdown profile">
         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <ClientOnly>
-                    <FontAwesomeIcon class="user" :icon="['far', 'user']" />
-            </ClientOnly>
+            <FontAwesomeIcon class="user" :icon="['far', 'user']" />
         </a>
         <div class="dropdown-menu dropdown-menu-end profile-dropdown-menu">
             <!-- Авторизован -->
-            <a class="dropdown-item" href="#">Кабинет</a>
-            <a class="dropdown-item" href="#">Выйти</a>
-            <!-- Не Авторизован -->
-            <div class="dropdown-item">
-                <button type="button" class="btn btn-primary">Войти</button>
+            <div v-if="user.auth.isAuth">
+                <a class="dropdown-item" href="#">Профиль</a>
+                <a class="dropdown-item" href="#">Выйти</a>
             </div>
-            <form class="px-3 py-3">
-                <div class="mb-3">
-                    <h4>Не зарегистрированы?</h4>
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Адрес электронной
-                        почты</label>
-                    <input type="email" class="form-control" id="email">
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Пароль</label>
-                    <input type="password" class="form-control" id="password">
-                </div>
-                <div class="mb-3">
-                    <label for="passwordConfirm" class="form-label">Подтвердить
-                        пароль</label>
-                    <input type="password" class="form-control" id="passwordConfirm">
-                </div>
-                <div class="mb-3">
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="dropdownCheck">
-                        <label class="form-check-label" for="dropdownCheck">
-                            <!-- TODO ссылка на политику -->
-                            Ознакомился с политикой конфединциальности
-                        </label>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Зарегистрироваться</button>
-            </form>
+            <!-- Не Авторизован -->
+            <div v-else>
+                <NuxtLink class="dropdown-item" to="/auth/login">Войти</NuxtLink>
+                <NuxtLink class="dropdown-item" to="/auth/registration">Зарегистрироваться</NuxtLink>
+            </div>
+            
         </div>
     </div>
 </template>
-
-<script setup>
-
-</script>
 
 <style lang="scss" scoped></style>
