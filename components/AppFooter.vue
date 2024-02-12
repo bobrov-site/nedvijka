4e1064e22bd3b59897458856d1830f49075acbcd
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-12 12:14:40 +0300

    footer fixed to bottom of page

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="py-3 mt-4 bg-primary">
+    <footer class="py-3 mt-auto bg-primary">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
             <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Главная</a></li>
             <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Поддержка</a></li>
