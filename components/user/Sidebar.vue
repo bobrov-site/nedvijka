9ec44d44db5fa7cafd13fd6b071796d4b777e725
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-12 12:15:27 +0300

    created sidebar component to user

---
+++ components/user/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="users-sidebar">
+        <div class="default-avatar bg-primary text-white fw-bold fs-1 rounded-circle d-flex justify-content-center align-items-center mb-2">
+            ИИ
+        </div>
+        <div class="d-flex justify-content-between mb-2">
+            <span class="fw-light">Имя:</span>
+            <span class="fw-bold">Иван Иванов</span>
+        </div>
+        <div class="d-flex justify-content-between">
+            <span class="fw-light">Дата регистрации:</span>
+            <span class="fw-bold">02.02.2024</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -1,13 +1,34 @@
-<template>
-    <div>
+<script setup>
+definePageMeta({
+    layout: 'profile'
+})
+</script>
 
+<template>
+    <div class="card mt-4">
+        <div class="card-body">
+            <h1 class="mb-4">Профиль</h1>
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <aside>
+                        <UserSidebar />
+                    </aside>
+                </div>
+                <div class="col-md-8">
+                    <main>
+                        <ul class="nav nav-tabs ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Мои объявления</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Избранное</a>
+                            </li>
+                        </ul>
+                    </main>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
