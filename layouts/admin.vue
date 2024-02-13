3757aa7eec503455d6d585a3eebd393d5b6aea3a
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-13 15:36:13 +0300

    added sidebar to admin. Added admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="app">
+        <div class="container-fluid">
+            <slot />
+        </div>
+        <AppFooter />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
