aaf7e77dfc0d3fda1ec5a97bc911e6ba29134969
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-13 17:20:33 +0300

    added tabs to admin

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,7 +1,17 @@
 <template>
     <div class="app">
-        <div class="container-fluid">
-            <slot />
+        <div class="container-fluid p-0">
+            <div class="row g-0">
+                <div class="col-12 col-md-3">
+                    <AdminSidebar/>
+                </div>
+                <div class="col-12 col-md-9">
+                    <div class="p-4">
+                        <slot />
+                    </div>
+                </div>
+            </div>
+            
         </div>
         <AppFooter />
     </div>
