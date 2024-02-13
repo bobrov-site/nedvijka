bfc5238bcf765c50653266f7ec052d2477e9311e
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-13 15:22:53 +0300

    added for admin nav

--- stores/user.js
+++ stores/user.js
@@ -2,6 +2,7 @@ export const useUserStore = defineStore('userStore', {
     state: () => ({
         auth: {
             isAuth: true,
+            isAdmin: true,
         },
         activeTab: 1
     }),
