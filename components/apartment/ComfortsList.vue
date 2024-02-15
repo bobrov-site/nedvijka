65c7e384cdbfada876e9a92077f59322f9591599
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-15 15:10:52 +0300

    created ComfortsList component

---
+++ components/apartment/ComfortsList.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul class="list-group max-w-100 comforts-list">
+        <li v-for="(comfort, index) in props.comfortsList" :key="index" class="list-group-item d-flex align-items-center justify-content-between">
+            <span>{{ comfort }}</span>
+            <FontAwesomeIcon class="square-check-icon text-success" :icon="['far', 'square-check']" />
+        </li>
+    </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+    comfortsList: {
+        type: Array
+    }
+})
+</script>
+
+<style lang="scss" scoped></style>
