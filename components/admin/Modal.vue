c3c71228e783601858a5c01e161a127c59188796
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-15 15:20:17 +0300

    created Modal in admin

---
+++ components/admin/Modal.vue
@@ -0,0 +1,94 @@
+<script setup>
+const comfortsList = [
+    'Кондиционер',
+    'Фен',
+    'Косметический ремонт',
+    'Телевизор',
+    'Электрический чайник',
+]
+defineProps({
+    isEditable: {
+        type: Boolean,
+    }   
+})
+</script>
+
+<template>
+    <div class="apartmentModal modal fade" id="apartmentModal" tabindex="-1" aria-labelledby="apartmentModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="apartmentModalLabel">Подробная информация о квартире</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-2">
+                        <h2 class="modal-title fs-4">Название:</h2>
+                        <p class="modal-text">Название квартиры</p>
+                    </div>
+                    <div class="mb-2">
+                        <h2 class="modal-title fs-4">Описание:</h2>
+                        <p class="modal-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore dignissimos
+                            ex praesentium harum sequi dicta vel consectetur cum accusamus iusto quas molestiae libero,
+                            vitae odio in ipsum eveniet ratione quisquam!</p>
+                    </div>
+                    <div class="mb-2">
+                        <h2 class="modal-title fs-4">Изображения:</h2>
+                        <div class="row apartments-modal-images g-3">
+                            <div class="col-4">
+                                <NuxtImg class="apartments-modal-img" src="/images/apartment_1.jpg" alt="Квартира"
+                                    sizes="md:900px" />
+                            </div>
+                            <div class="col-4">
+                                <NuxtImg class="apartments-modal-img" src="/images/apartment_2.jpg" alt="Квартира"
+                                    sizes="md:900px" />
+                            </div>
+                            <div class="col-4">
+                                <NuxtImg class="apartments-modal-img" src="/images/apartment_3.jpg" alt="Квартира"
+                                    sizes="md:900px" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-2">
+                        <h2 class="modal-title fs-4">Количество комнат:</h2>
+                        <p class="modal-text">2</p>
+                    </div>
+                    <div class="mb-2">
+                        <h2 class="modal-title fs-4">Количество кроватей:</h2>
+                        <p class="modal-text">2</p>
+                    </div>
+                    <div class="mb-2">
+                        <h2 class="modal-title fs-4">Количество квадратов:</h2>
+                        <p class="modal-text">42 кв.м</p>
+                    </div>
+                    <div class="mb-2">
+                        <h2 class="modal-title fs-4">Максимальное количество гостей:</h2>
+                        <p class="modal-text">4</p>
+                    </div>
+                    <div class="mb-2">
+                        <h2 class="modal-title fs-4">Город:</h2>
+                        <p class="modal-text">Ставрополь</p>
+                    </div>
+                    <div class="mb-2">
+                        <h2 class="modal-title fs-4">Адрес:</h2>
+                        <p class="modal-text">Рогожникова 7</p>
+                    </div>
+                    <div class="mb-2">
+                        <h2 class="modal-title fs-4">Удобства:</h2>
+                        <ApartmentComfortsList :comfortsList="comfortsList" />
+                    </div>
+                    <div class="mb-2">
+                        <h2 class="modal-title fs-4">Цена сутки:</h2>
+                        <p class="modal-text">2 033₽</p>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
--- pages/admin/apartments.vue
+++ pages/admin/apartments.vue
@@ -1,4 +1,13 @@
+<script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+definePageMeta({
+    layout: 'admin'
+})
+</script>
+
 <template>
+    <AdminModal/>
     <div class="table-responsive">
         <table class="table">
             <thead>
@@ -9,7 +18,7 @@
                     <th scope="col">Пользователь</th>
                     <th scope="col">ID Пользователя</th>
                     <th scope="col">Подробнее</th>
-                    <th scope="col">Опубликован</th>
+                    <th scope="col">Действия</th>
                     <th scope="col">Дата и время публикации</th>
                 </tr>
             </thead>
@@ -17,58 +26,67 @@
                 <tr>
                     <th scope="row">1</th>
                     <td>Название квартиры</td>
-                    <td>Рогожникова 7</td>
+                    <td>г. Ставрополь, Рогожникова 7</td>
                     <td>Иван Иванов</td>
                     <td>1</td>
                     <td>
-                        <button class="btn btn-outline-primary">Подробнее</button>
+                        <button class="btn btn-outline-primary" data-bs-toggle="modal"
+                            data-bs-target="#apartmentModal">Подробнее</button>
                     </td>
                     <td>
-                        <button type="button" class="btn btn-outline-success me-4" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Опубликовать квартиру">
-                            <FontAwesomeIcon :icon="['fas', 'check']"/>
-                        </button>
-                        <button type="button" class="btn btn-outline-danger" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Удалить квартиру из базы">
-                            <FontAwesomeIcon :icon="['fas', 'trash-can']"/>
-                        </button>
-                        <span class="badge text-bg-danger">Не опубликован</span>
+                        <div class="d-flex">
+                            <button type="button" class="btn btn-outline-success me-4" data-bs-toggle="tooltip"
+                                data-bs-placement="top" data-bs-title="Опубликовать квартиру">
+                                <FontAwesomeIcon :icon="['fas', 'check']" />
+                            </button>
+                            <button type="button" class="btn btn-outline-primary me-4" data-bs-toggle="tooltip"
+                                data-bs-placement="top" data-bs-title="Редактировать квартиру">
+                                <FontAwesomeIcon :icon="['fas', 'edit']" />
+                            </button>
+                            <button type="button" class="btn btn-outline-danger" data-bs-toggle="tooltip"
+                                data-bs-placement="top" data-bs-title="Удалить квартиру из базы">
+                                <FontAwesomeIcon :icon="['fas', 'trash-can']" />
+                            </button>
+                        </div>
                     </td>
                     <td>
                         02.02.2024 16:54
+                        <span class="badge text-bg-danger">Не опубликован</span>
                     </td>
                 </tr>
                 <tr>
                     <th scope="row">2</th>
                     <td>Название квартиры</td>
-                    <td>Рогожникова 11</td>
+                    <td>г.Ставрополь, Рогожникова 11</td>
                     <td>Петр Петрович</td>
                     <td>2</td>
                     <td>
-                        <button type="button" class="btn btn-outline-primary">Подробнее</button>
+                        <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal"
+                            data-bs-target="#apartmentModal">Подробнее</button>
                     </td>
                     <td>
-                        <button type="button" class="btn btn-outline-secondary me-4" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Добавить квартиру в архив">
-                            <FontAwesomeIcon :icon="['fas', 'box-archive']"/>
-                        </button>
-                        <button type="button" class="btn btn-outline-danger" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Удалить квартиру из базы">
-                            <FontAwesomeIcon :icon="['fas', 'trash-can']"/>
-                        </button>
-                        <span class="badge text-bg-success">Опубликован</span>
+                        <div class="d-flex">
+                            <button type="button" class="btn btn-outline-secondary me-4" data-bs-toggle="tooltip"
+                                data-bs-placement="top" data-bs-title="Добавить квартиру в архив">
+                                <FontAwesomeIcon :icon="['fas', 'box-archive']" />
+                            </button>
+                            <button type="button" class="btn btn-outline-primary me-4" data-bs-toggle="tooltip"
+                                data-bs-placement="top" data-bs-title="Редактировать квартиру">
+                                <FontAwesomeIcon :icon="['fas', 'edit']" />
+                            </button>
+                            <button type="button" class="btn btn-outline-danger" data-bs-toggle="tooltip"
+                                data-bs-placement="top" data-bs-title="Удалить квартиру из базы">
+                                <FontAwesomeIcon :icon="['fas', 'trash-can']" />
+                            </button>
+                        </div>
                     </td>
                     <td>
                         02.02.2024 16:54
+                        <span class="badge text-bg-success">Опубликован</span>
                     </td>
                 </tr>
             </tbody>
         </table>
     </div>
 </template>
-
-<script setup>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-definePageMeta({
-    layout: 'admin'
-})
-</script>
-
 <style lang="scss" scoped></style>
