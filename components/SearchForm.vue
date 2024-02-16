048eed429a579d889677a4ec3725e307b1b1202c
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-16 14:27:27 +0300

    changed searchForm

--- components/SearchForm.vue
+++ components/SearchForm.vue
@@ -1,35 +1,37 @@
 <script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 const props = defineProps({
     formTitle: {
         type: String,
         required: true,
     },
-    isProcecced: {
-        type: Boolean,
-    }
 })
 const date = ref()
 const start = ref()
 const end = ref()
+const adult = ref(1)
+const children = ref(0)
+const isShowDropdown = ref(false)
 
 const handleDate = (modelData) => {
     date.value = modelData;
     start.value = parseDate(modelData[0])
     end.value = parseDate(modelData[1])
 }
+
 </script>
 
 <template>
-    <div class="card p-4 search-form h-100" :class="{'position-sticky top-0' : isProcecced}">
-        <div class="row mb-3">
+    <div class="row mb-3">
             <div class="col-12">
                 <h2>{{ props.formTitle }}</h2>
             </div>
-        </div>
+    </div>
         <form class="row g-3">
             <div class="col-md-6">
                 <label for="datalist" class="form-label">Выберите город</label>
-                <input class="form-control" list="datalistOptions" id="datalist" placeholder="Ставрополь">
+                <input class="form-control" list="datalistOptions" id="datalist">
                 <datalist id="datalistOptions">
                     <option value="Ставрополь" />
                     <option value="Пятигорск" />
@@ -38,9 +40,18 @@ const handleDate = (modelData) => {
             </div>
             <div class="col-md-6">
                 <label for="guests" class="form-label">Гости</label>
-                <button type="button" class="form-control dropdown-toggle text-end" id="guests" data-bs-toggle="dropdown"
-                    data-bs-auto-close="false" aria-expanded="false"></button>
-                <ul class="dropdown-menu">
+                <div class="position-relative">
+                    <button @click="isShowDropdown = !isShowDropdown" type="button" class="form-control text-end" id="guests">
+                        <span class="d-block w-100 text-start">
+                            {{ adult }} взрослых, {{ children }} детей
+                        </span>
+                        <template>
+                            <FontAwesomeIcon class="caret-down-icon position-absolute" :icon="['fas', 'caret-down']"/>
+                        </template>
+                    </button>
+                </div>
+
+                <ul :class="{ 'show': isShowDropdown }" class="dropdown-menu">
                     <li class="dropdown-item">
                         <div class="row g-3">
                             <div class="col">
@@ -48,9 +59,9 @@ const handleDate = (modelData) => {
                                 <span>От 18 лет</span>
                             </div>
                             <div class="col">
-                                <button type="button" class="btn-counter rounded-circle border-0 fs-4 me-2">-</button>
-                                <span>33</span>
-                                <button type="button" class="btn-counter rounded-circle border-0 fs-4 ms-2">+</button>
+                                <button @click="--adult" :disabled="adult === 0" type="button" class="btn-counter rounded-circle border-0 fs-4 me-2">-</button>
+                                <span>{{ adult }}</span>
+                                <button @click="++adult" :disabled="adult === 4" type="button" class="btn-counter rounded-circle border-0 fs-4 ms-2">+</button>
                             </div>
                         </div>
                     </li>
@@ -64,9 +75,9 @@ const handleDate = (modelData) => {
                                 <span>От 0 до 17 лет</span>
                             </div>
                             <div class="col">
-                                <button type="button" class="btn-counter rounded-circle border-0 fs-4 me-2">-</button>
-                                <span>33</span>
-                                <button type="button" class="btn-counter rounded-circle border-0 fs-4 ms-2">+</button>
+                                <button @click="--children" :disabled="adult === 0" type="button" class="btn-counter rounded-circle border-0 fs-4 me-2">-</button>
+                                <span>{{ children }}</span>
+                                <button @click="++children" :disabled="adult === 4" type="button" class="btn-counter rounded-circle border-0 fs-4 ms-2">+</button>
                             </div>
                         </div>
                     </li>
@@ -75,15 +86,15 @@ const handleDate = (modelData) => {
                     </li>
                     <li class="dropdown-item">
                         <div class="d-grid">
-                            <button class="btn btn-primary ">Изменить</button>
+                            <button @click="isShowDropdown = false" type="button" class="btn btn-primary ">Сохранить</button>
                         </div>
                     </li>
                 </ul>
             </div>
             <div class="col-md-6">
                 <label for="start" class="form-label">Заезд</label>
-                <VueDatePicker v-model="date" @update:model-value="handleDate" :auto-position="false" auto-apply
-                    id="start" range text-input :enable-time-picker="false" :clearable="false" locale="ru">
+                <VueDatePicker v-model="date" @update:model-value="handleDate" :auto-position="false" auto-apply id="start"
+                    range text-input :enable-time-picker="false" :clearable="false" locale="ru">
 
                     <template #dp-input>
                         <input type="text" :value="start" class="form-control">
@@ -95,8 +106,8 @@ const handleDate = (modelData) => {
             </div>
             <div class="col-md-6">
                 <label for="end" class="form-label">Выезд</label>
-                <VueDatePicker v-model="date" @update:model-value="handleDate" :auto-position="false" auto-apply
-                    id="end" range text-input :enable-time-picker="false" locale="ru" :clearable="false">
+                <VueDatePicker v-model="date" @update:model-value="handleDate" :auto-position="false" auto-apply id="end"
+                    range text-input :enable-time-picker="false" locale="ru" :clearable="false">
                     <template #dp-input>
                         <input type="text" :value="end" class="form-control">
                     </template>
@@ -105,28 +116,10 @@ const handleDate = (modelData) => {
                     </template>
                 </VueDatePicker>
             </div>
-            <div v-if="!isProcecced" class="col-12">
-                <!--TODO добавить изменение кнопки только в том случае, если данные были получены--> 
-                <button v-if="!date" type="submit" class="btn btn-lg btn-primary w-100">Поиск</button>
-                <button v-else type="submit" class="btn btn-lg btn-primary w-100">Изменить</button>
-            </div>
-            <div v-if="isProcecced" class="col-12">
-                <h4>Стоимость:</h4>
-                <div class="d-flex justify-content-between mb-2">
-                    <span class="fw-light">Итого за сутки:</span>
-                    <span class="fw-light">2 033₽</span>
-                </div>
-                <div class="d-flex justify-content-between mb-2">
-                    <span class="fw-bold text-primary text-uppercase">Итого всего:</span>
-                    <span class="fw-bold text-primary text-uppercase">5 033₽</span>
-                </div>
-                <p class="card-text text-secondary fw-lighter fs-6">
-                    Нажимая кнопку Забронировать, вы соглашаетесь с условиями <a href="#">пользовательского соглашения</a> и на <a href="#">обработку персональных данных</a>
-                </p>
-                <button class="btn btn-primary w-100">Забронировать</button>
+            <div class="col-12">
+                <button type="submit" class="btn btn-lg btn-primary w-100">Поиск</button>
             </div>
         </form>
-    </div>
 </template>
 
 <style lang="scss" scoped></style>
