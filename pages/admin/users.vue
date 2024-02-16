3fd417823a7b8a7bfa456dd4a10eb2ae3a70cff0
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-16 12:30:39 +0300

    added users table

--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -1,6 +1,29 @@
 <template>
-    <div>
-
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID пользователя</th>
+                    <th scope="col">Имя</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Дата и время регистрации</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th class="bg-danger text-white" scope="row">1</th>
+                    <td class="bg-danger text-white">Иван Иванов</td>
+                    <td class="bg-danger text-white">example@example.com</td>
+                    <td class="bg-danger text-white">02.02.2024 16:54</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Петр Петров</td>
+                    <td>example@example.com</td>
+                    <td>02.03.2024 16:56</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
