86e479ac21cc630165ff0f393980cb4ca06dda67
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-19 18:43:24 +0300

    added support page

---
+++ pages/support.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-6">
+            <div class="card-support card">
+                <div class="card-body">
+                    <h1 class="card-title">Поддержка серсива Fridda</h1>
+                    <div class="mb-2">
+                        Вы можете позвонить по номеру + 7(962)-425-88-45
+                    </div>
+                    <div class="mb-2">
+                        Написать нам на почту <a href="mailto:example@example.com">example@example.com</a>
+                    </div>
+                    <div class="mb-2">
+                        У нас так же есть jivochat. Он находится в углу экрана
+                    </div>
+                    <hr>
+                    <div>
+                        <h2 class="fs-4">Или напишите напрямую заполнив форму ниже</h2>
+                    </div>
+                    <form class="mb-2 form-support">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Адрес вашей электронной почты</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1">
+                        </div>
+                        <div class="mb-3">
+                            <label for="text" class="form-label">Текст обращения</label>
+                            <textarea class="form-control" id="text" rows="4"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary mb-2">Отправить</button>
+                        <p class="fw-light">
+                            Нажимая на кнопку отправить, вы соглашаетесь с условиями <a href="#">пользовательского
+                                соглашения</a> и
+                            на <a href="#">обработку персональных данных</a>
+                        </p>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
