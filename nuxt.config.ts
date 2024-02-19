4b67cd269bf05a57c6e9018a350a3cfd6cb73e51
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-19 16:58:17 +0300

    change font awesome type all

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   ],
   plugins: [
     { src: '~/plugins/useBootstrap.ts', mode: 'client'},
-    {src: '~/plugins/useFontAwesome.ts', mode: 'client',},
+    {src: '~/plugins/useFontAwesome.ts', mode: 'all',},
     {src: '~/plugins/useVueDatePicker.ts', mode: 'all',},
     {src: '~/plugins/useVueYandexMaps.ts', mode: 'all',},
   ],
