d52b3041baf67bca465be7d92ab774bae0fa8c74
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-20 12:27:47 +0300

    change title for authModal

--- components/AuthModal.vue
+++ components/AuthModal.vue
@@ -3,7 +3,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5">Войти или создать аккаунт</h1>
+                    <h1 class="modal-title fs-5">Чтобы воспользоваться всеми возможностями сервиса нужно войти или зарегистрироваться</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
