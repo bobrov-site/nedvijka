8bf04fcb39d8d65c48190e435036b9aa5bfc862e
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-20 14:02:54 +0300

    change navbar for user

--- components/user/Navbar.vue
+++ components/user/Navbar.vue
@@ -1,21 +1,23 @@
 <template>
-    <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <button @click="changeTab(1)" :class="{'active': user.activeTab === 1}" class="nav-link" type="button">Мои квартиры</button>
-        </li>
-        <li class="nav-item">
-            <button @click="changeTab(0)" :class="{'active': user.activeTab === 0}" class="nav-link" type="button">Избранное</button>
-        </li>
-    </ul>
+    <div>
+        <ul v-if="user.auth.isGuest" class="nav nav-tabs">
+            <li class="nav-item">
+                <NuxtLink to="/guest/favorites" :class="{ 'active': route.name === 'guest-favorites' }" class="nav-link">
+                    Избранное
+                </NuxtLink>
+            </li>
+            <li class="nav-item">
+                <NuxtLink to="/guest/bookings" :class="{ 'active': route.name === 'guest-bookings' }" class="nav-link">
+                    Мои брони
+                </NuxtLink>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script setup>
 const user = useUserStore();
-const changeTab = (id) => {
-    if (id !== user.activeTab) {
-        user.activeTab = id
-    }
-}
+const route = useRoute();
 </script>
 
 <style lang="scss" scoped></style>
