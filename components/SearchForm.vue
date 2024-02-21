fed4ed2cfbdc1f447d293b11ede2914235e434ac
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-21 13:56:42 +0300

    format searchForm.vue

--- components/SearchForm.vue
+++ components/SearchForm.vue
@@ -24,76 +24,80 @@ const handleDate = (modelData) => {
 
 <template>
     <div class="row mb-3">
-            <div class="col-12">
-                <h2>{{ props.formTitle }}</h2>
-            </div>
+        <div class="col-12">
+            <h2>{{ props.formTitle }}</h2>
+        </div>
     </div>
-        <form class="row g-3">
-            <div class="col-md-6">
-                <label for="datalist" class="form-label">Выберите город</label>
-                <input class="form-control" list="datalistOptions" id="datalist">
-                <datalist id="datalistOptions">
-                    <option value="Ставрополь" />
-                    <option value="Пятигорск" />
-                    <option value="Кисловодск" />
-                </datalist>
-            </div>
-            <div class="col-md-6">
-                <label for="guests" class="form-label">Гости</label>
-                <div class="position-relative">
-                    <button @click="isShowDropdown = !isShowDropdown" type="button" class="form-control text-end" id="guests">
-                        <span class="d-block w-100 text-start">
-                            {{ adult }} взрослых, {{ children }} детей
-                        </span>
-                        <FontAwesomeIcon class="caret-down-icon position-absolute" :icon="['fas', 'caret-down']"/>
-                        
-                    </button>
-                </div>
+    <form class="row g-3">
+        <div class="col-md-6">
+            <label for="datalist" class="form-label">Выберите город</label>
+            <input class="form-control" list="datalistOptions" id="datalist">
+            <datalist id="datalistOptions">
+                <option value="Ставрополь" />
+                <option value="Пятигорск" />
+                <option value="Кисловодск" />
+            </datalist>
+        </div>
+        <div class="col-md-6">
+            <label for="guests" class="form-label">Гости</label>
+            <div class="position-relative">
+                <button @click="isShowDropdown = !isShowDropdown" type="button" class="form-control text-end" id="guests">
+                    <span class="d-block w-100 text-start">
+                        {{ adult }} взрослых, {{ children }} детей
+                    </span>
+                    <FontAwesomeIcon class="caret-down-icon position-absolute" :icon="['fas', 'caret-down']" />
 
-                <ul :class="{ 'show': isShowDropdown }" class="dropdown-menu">
-                    <li class="dropdown-item">
-                        <div class="row g-3">
-                            <div class="col">
-                                <h5>Взрослые</h5>
-                                <span>От 18 лет</span>
-                            </div>
-                            <div class="col">
-                                <button @click="--adult" :disabled="adult === 0" type="button" class="btn-counter rounded-circle border-0 fs-4 me-2">-</button>
-                                <span>{{ adult }}</span>
-                                <button @click="++adult" :disabled="adult === 4" type="button" class="btn-counter rounded-circle border-0 fs-4 ms-2">+</button>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li class="dropdown-item">
-                        <div class="row g-3">
-                            <div class="col">
-                                <h5>Дети</h5>
-                                <span>От 0 до 17 лет</span>
-                            </div>
-                            <div class="col">
-                                <button @click="--children" :disabled="adult === 0" type="button" class="btn-counter rounded-circle border-0 fs-4 me-2">-</button>
-                                <span>{{ children }}</span>
-                                <button @click="++children" :disabled="adult === 4" type="button" class="btn-counter rounded-circle border-0 fs-4 ms-2">+</button>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li class="dropdown-item">
-                        <div class="d-grid">
-                            <button @click="isShowDropdown = false" type="button" class="btn btn-primary ">Сохранить</button>
-                        </div>
-                    </li>
-                </ul>
+                </button>
             </div>
-            <div class="col-md-6">
-                <label for="start" class="form-label">Заезд</label>
-                <ClientOnly>
-                    <VueDatePicker v-model="date" @update:model-value="handleDate" :auto-position="false" auto-apply id="start"
+
+            <ul :class="{ 'show': isShowDropdown }" class="dropdown-menu">
+                <li class="dropdown-item">
+                    <div class="row g-3">
+                        <div class="col">
+                            <h5>Взрослые</h5>
+                            <span>От 18 лет</span>
+                        </div>
+                        <div class="col">
+                            <button @click="--adult" :disabled="adult === 0" type="button"
+                                class="btn-counter rounded-circle border-0 fs-4 me-2">-</button>
+                            <span>{{ adult }}</span>
+                            <button @click="++adult" :disabled="adult === 4" type="button"
+                                class="btn-counter rounded-circle border-0 fs-4 ms-2">+</button>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li class="dropdown-item">
+                    <div class="row g-3">
+                        <div class="col">
+                            <h5>Дети</h5>
+                            <span>От 0 до 17 лет</span>
+                        </div>
+                        <div class="col">
+                            <button @click="--children" :disabled="adult === 0" type="button"
+                                class="btn-counter rounded-circle border-0 fs-4 me-2">-</button>
+                            <span>{{ children }}</span>
+                            <button @click="++children" :disabled="adult === 4" type="button"
+                                class="btn-counter rounded-circle border-0 fs-4 ms-2">+</button>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li class="dropdown-item">
+                    <div class="d-grid">
+                        <button @click="isShowDropdown = false" type="button" class="btn btn-primary ">Сохранить</button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <label for="start" class="form-label">Заезд</label>
+            <ClientOnly>
+                <VueDatePicker v-model="date" @update:model-value="handleDate" :auto-position="false" auto-apply id="start"
                     range text-input :enable-time-picker="false" :clearable="false" locale="ru">
 
                     <template #dp-input>
@@ -103,11 +107,12 @@ const handleDate = (modelData) => {
                         <FontAwesomeIcon v-if="!start" class="datepicker-calendar-icon" :icon="['far', 'calendar']" />
                     </template>
                 </VueDatePicker>
-                </ClientOnly>
-                
-            </div>
-            <div class="col-md-6">
-                <label for="end" class="form-label">Выезд</label>
+            </ClientOnly>
+
+        </div>
+        <div class="col-md-6">
+            <label for="end" class="form-label">Выезд</label>
+            <ClientOnly>
                 <VueDatePicker v-model="date" @update:model-value="handleDate" :auto-position="false" auto-apply id="end"
                     range text-input :enable-time-picker="false" locale="ru" :clearable="false">
                     <template #dp-input>
@@ -117,11 +122,13 @@ const handleDate = (modelData) => {
                         <FontAwesomeIcon v-if="!end" class="datepicker-calendar-icon" :icon="['far', 'calendar']" />
                     </template>
                 </VueDatePicker>
-            </div>
-            <div class="col-12">
-                <button type="submit" class="btn btn-lg btn-primary w-100">Поиск</button>
-            </div>
-        </form>
+            </ClientOnly>
+
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-lg btn-primary w-100">Поиск</button>
+        </div>
+    </form>
 </template>
 
 <style lang="scss" scoped></style>
