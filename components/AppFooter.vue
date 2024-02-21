369bae0857ba1c4b652b61b53c83e359601037ce
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-21 09:44:10 +0300

    added new items in footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,11 +1,21 @@
 <template>
     <footer class="py-3 mt-auto bg-primary">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Главная</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Поддержка</a></li>
+            <li class="nav-item">
+                <NuxtLink class="nav-link px-2 text-light" to="/">Главная</NuxtLink>
+            </li>
+            <li class="nav-item">
+                <NuxtLink class="nav-link px-2 text-light" to="/support">Поддержка</NuxtLink>
+            </li>
+            <li class="nav-item">
+                <NuxtLink class="nav-link px-2 text-light" to="/">Пользовательское соглашение</NuxtLink>
+            </li>
+            <li class="nav-item">
+                <NuxtLink class="nav-link px-2 text-light" to="/">Обработка персональных данных</NuxtLink>
+            </li>
             <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Личный кабинет</a></li>
         </ul>
-        <p class="text-center text-light">© 2023 Company, Inc</p>
+        <p class="text-center text-light">© 2024 Fridda</p>
     </footer>
 </template>
 
