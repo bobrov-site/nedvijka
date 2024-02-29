6f8260972b1c5e3459d5d653cda3f9d9a7d83e44
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-29 13:10:27 +0300

    reset auth state. Added user id and email. Added authUser action

--- stores/user.js
+++ stores/user.js
@@ -1,13 +1,21 @@
 export const useUserStore = defineStore('userStore', {
     state: () => ({
         auth: {
-            isAuth: true,
+            isAuth: false,
             isAdmin: false,
             isGuest: false,
-            isRenter: true,
+            isRenter: false,
         },
+        user: {
+            id: null,
+            email: null,
+        }
     }),
     actions: {
-
+        authUser(user) {
+            this.auth.isAuth = true
+            this.user.id = user._id
+            this.user.email = user.email
+        }
     }
 })
