38d13510ceea53e3d16bdb00a5c667ffd44f1c94
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-29 13:09:59 +0300

    added auth logic to front. Login and redirect if user not found

--- components/AuthModal.vue
+++ components/AuthModal.vue
@@ -1,22 +1,57 @@
 <script setup>
+import { signIn } from 'next-auth/react';
+
+const user = useUserStore();
+
 const form = ref({
     email: '',
+    password: '',
+})
+
+const errors = ref({
+    errorRequired: false,
+    errorPasswordWrong: false,
+    errorPasswordLength: false,
+    errorPasswordNotMatch: false
 })
 
 const isButtonDisabled = ref(false);
-// const isEmailExist = ref(null);
-const { signIn } = useAuth();
-const checkEmail = async($event) => {
+const isEmailExist = ref(null);
+const checkEmail = async ($event) => {
     isButtonDisabled.value = true;
     try {
-        await signIn("credentials", form.value);
+        await $fetch('/api/auth/check', {
+            method: 'POST',
+            body: { email: form.value.email }
+        })
+        isButtonDisabled.value = false;
+        // TODO добавить редирект на страницу ошибки
+    }
+    catch (e) {
+        isEmailExist.value = true
         isButtonDisabled.value = false;
     }
-    catch(e) {
-        console.log(e, 'error')
-        isButtonDisabled.value = false;
-    }
-    
+}
+
+const errorsCodes = getErrorsCodes()
+const resetErrors = () => {
+    Object.keys(errors.value).forEach((key) => errors.value[key] = false)
+}
+
+const logIn = () => {
+    resetErrors();
+    signIn('credentials', {email: form.value.email, password: form.value.password, redirect: false})
+    .then((credentials) => {
+        if (credentials.error) {
+            errors.value[credentials.error] = true
+            return
+        }
+        const { getSession } = useAuth();        
+        getSession().then((session) => {
+            user.authUser(session.user);
+            navigateTo('/'); 
+        })
+    })
 }
 </script>
 <template>
@@ -24,15 +59,29 @@ const checkEmail = async($event) => {
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5">Чтобы воспользоваться всеми возможностями сервиса нужно войти или зарегистрироваться</h1>
+                    <h1 class="modal-title fs-5">Чтобы воспользоваться всеми возможностями сервиса нужно войти или
+                        зарегистрироваться</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form @submit.prevent="checkEmail($event)">
+                    <form v-if="!isEmailExist" @submit.prevent="checkEmail($event)">
                         <div class="mb-3">
                             <label for="email" class="form-label">Адрес электронной
                                 почты</label>
                             <input v-model="form.email" type="email" class="form-control" id="email">
+                        </div>
+                        <button :disabled="isButtonDisabled" type="submit" class="btn btn-primary">Далее</button>
+                    </form>
+                    <form v-if="isEmailExist" @submit.prevent="logIn()">
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Адрес электронной
+                                почты</label>
+                            <input v-model="form.email" type="email" class="form-control" id="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Пароль</label>
+                            <input v-model="form.password" type="password" required :class="{'is-invalid': errors.errorPasswordWrong}" class="form-control" id="password">
+                            <div class="invalid-feedback">{{ errorsCodes.errorPasswordWrong }}</div>
                         </div>
                         <button :disabled="isButtonDisabled" type="submit" class="btn btn-primary">Войти</button>
                     </form>
