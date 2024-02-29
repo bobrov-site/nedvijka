42afd576e0136b9e71e83e60c493dea95503d8d0
Daniil Bobrov <allodrmen@yandex.ru>
2024-02-29 13:07:33 +0300

    added errors codes

---
+++ utils/getErrorsCodes.js
@@ -0,0 +1,9 @@
+export default (() => {
+    return {
+        errorRequired: 'Это поле обязательное',
+        errorPasswordNotMatch: 'Пароль подтверждения должен совпадать',
+        errorPasswordLength: 'Пароль должен быть минимум из 6 символов',
+        errorPasswordWrong: 'Неправильный пароль',
+        
+    }
+})
