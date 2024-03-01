ccda7215156a11dee778809fdc2517fe75cc0c2e
Daniil Bobrov <allodrmen@yandex.ru>
2024-03-01 18:21:36 +0300

    added admin role

--- components/user/Navbar.vue
+++ components/user/Navbar.vue
@@ -12,7 +12,7 @@
                 </NuxtLink>
             </li>
         </ul>
-        <ul v-if="user.user.role === 'renter'" class="nav nav-tabs">
+        <ul v-if="user.user.role === 'renter' || user.user.role === 'admin'" class="nav nav-tabs">
             <li class="nav-item">
                 <NuxtLink to="/renter/objects" :class="{ 'active': route.name === 'renter-objects' }" class="nav-link">
                     Мои обьекты
