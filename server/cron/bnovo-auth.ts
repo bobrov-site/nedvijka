6f1bb876264875af21deddba6ad063a2e7ca0a08
Daniil Bobrov <allodrmen@yandex.ru>
2024-03-06 12:31:15 +0300

    change time for cron

--- server/cron/bnovo-auth.ts
+++ server/cron/bnovo-auth.ts
@@ -1,11 +1,11 @@
 import { defineCronHandler } from '#nuxt/cron';
 
-export default defineCronHandler('everyThirtyMinutes', () => {
+export default defineCronHandler('everyTenMinutes', () => {
     $fetch('/bnovo/auth', {method: 'GET'})
     .then(() => {
         console.log('bnovo auth success')
     })
     .catch((e) => {
-        console.warn('bnovo auth failed')
+        console.log('bnovo auth failed')
     })
 }, {runOnInit: true})
