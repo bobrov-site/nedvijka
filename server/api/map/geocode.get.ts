28899a99825f18b2d5b8c9cbab2ba39bf68a1889
Daniil Bobrov <allodrmen@yandex.ru>
2024-03-07 18:27:50 +0300

    added geocode.get.ts

---
+++ server/api/map/geocode.get.ts
@@ -0,0 +1,32 @@
+import axios from "axios"
+
+export default defineEventHandler(async (event) => {
+    const { geocode } = getQuery(event)
+    if (!geocode) {
+        throw createError({
+            statusCode: 400,
+            statusMessage: 'Geocode is required'
+        })
+    }
+    try {
+        const response = await axios.get('https://geocode-maps.yandex.ru/1.x', {
+            params: {
+                apikey: 'd03416db-7b20-44b0-a7c2-297b4e834328',
+                geocode: geocode,
+                lang: 'ru_RU',
+                format: 'json'
+            }
+        })
+        console.log(response, 'response')
+        return {
+            geocode: response.data
+        }
+    }
+    catch(e) {
+        console.log(e, 'erroor')
+        throw createError({
+            statusCode: 500,
+            statusMessage: e.message
+        })
+    }
+})
