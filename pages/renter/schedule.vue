9c64dca2e9e718b0e3dc902d60418bcc62e19249
Daniil Bobrov <allodrmen@yandex.ru>
2024-03-22 15:40:56 +0300

    removed google calendar. Added gantt

--- pages/renter/schedule.vue
+++ pages/renter/schedule.vue
@@ -1,12 +1,5 @@
 <template>
     <div class="schedule">
-        <GanttSchedule/>
-        <div v-if="process === 'loaded'" class="row mt-4">
-            <div class="col-12">
-                <span v-for="legend in legends" :key="legend.id" :class="`badge ${legend.backgroundColor}`">{{
-            legend.name }}</span>
-            </div>
-        </div>
         <div v-if="process === 'loading'" class="row mt-4">
             <div class="col-12">
                 <div class="spinner-border text-primary" role="status">
@@ -15,16 +8,11 @@
             </div>
         </div>
         <div v-if="process === 'loaded'" class="row mt-4">
-            <FullCalendar ref="calendar" :options="calendarOptions" />
+            <GanttSchedule :tasks="tasks" :firstDayCurrentMonth="formatDate(firstDayCurrentMonth)" :lastDayCurrentMonth="formatDate(lastDayCurrentMonth)" />
         </div>
     </div>
 </template>
 <script setup>
-import FullCalendar from '@fullcalendar/vue3'
-import interactionPlugin from '@fullcalendar/interaction'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import allLocales from '@fullcalendar/core/locales-all';
 
 definePageMeta({
     layout: 'user'
@@ -33,39 +21,7 @@ onMounted(async () => {
     await loadData();
 })
 
-const calendar = ref(null)
 const tasks = ref([])
-const calendarOptions = ref({
-    plugins: [interactionPlugin, timeGridPlugin, dayGridPlugin],
-    initialView: 'dayGridMonth',
-    nowIndicator: true,
-    editable: true,
-    locales: allLocales,
-    locale: 'ru',
-    headerToolbar: {
-        left: '',
-        center: 'title',
-        right: ''
-    },
-    customButtons: {
-        prevButton: {
-            text: 'Назад',
-            click: (async () => {
-                calendar.value.calendar.prev()
-                date.value = new Date(calendar.value.calendar.getDate())
-                await loadData();
-            })
-        },
-        nextButton: {
-            text: 'Вперед',
-            click: (async () => {
-                calendar.value.calendar.next()
-                date.value = new Date(calendar.value.calendar.getDate())
-                await loadData();
-            })
-        }
-    }
-})
 const markersColors = {
     0: 'red',
     1: 'green',
@@ -80,26 +36,6 @@ const date = ref(new Date())
 const firstDayCurrentMonth = ref(null);
 const lastDayCurrentMonth = ref(null);
 const bookings = ref()
-const legends = ref([])
-const getCalendarLegend = () => {
-    const ids = []
-    legends.value = tasks.value
-        .filter((task) => {
-            if (ids.includes(task.id)) {
-                return false
-            }
-            ids.push(task.id)
-            return true
-        })
-        .map((item) => {
-            apartments.value.forEach((apartment) => {
-                if (apartment.id === item.id) {
-                    item = { ...item, ...apartment }
-                }
-            })
-            return item
-        })
-}
 const formatDate = (date) => {
     const day = date.getDate().toString().padStart(2, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0'); // getMonth() is zero-indexed
@@ -120,7 +56,24 @@ const loadData = async () => {
                     end: formatDate(lastDayCurrentMonth.value),
                 }
             })
-            setMarkersBnovo();
+            if (!bookings.value.bookings.result) {
+                return
+            }
+            const items = bookings.value.bookings.result.map((booking) => {
+                const item = {}
+                item.price = booking.amount;
+                item.time = {
+                    start : booking.real_arrival_format,
+                    end: booking.real_departure_format
+                }
+                item.id = booking.dual_roomtype_id
+                item.backgroundColor = markersColors[booking.dual_roomtype_id % 5]
+                item.title = `${booking.customer}`
+                item.editable = false
+                item.name = apartments.value.find((aparment) => aparment.id === booking.dual_roomtype_id).name
+                return item
+            })
+            tasks.value = items
             process.value = 'loaded'
         }
         catch (e) {
@@ -130,32 +83,6 @@ const loadData = async () => {
 
     }
     //TODO добавить способ подругзки от пользователей не админ 
-}
-
-const setMarkersBnovo = () => {
-    const changeFormatDate = (date) => {
-        const day = date.split('-')[0]
-        const month = date.split('-')[1]
-        const year = date.split('-')[2]
-        return `${year}-${month}-${day}`;
-    }
-    if (!bookings.value.bookings.result) {
-        return
-    }
-    const items = bookings.value.bookings.result.map((booking) => {
-        const item = {}
-        item.price = booking.amount;
-        item.start = changeFormatDate(booking.real_arrival_format.split(' ')[0])
-        item.end = changeFormatDate(booking.real_departure_format.split(' ')[0])
-        item.id = booking.dual_roomtype_id
-        item.backgroundColor = markersColors[booking.dual_roomtype_id % 5]
-        item.title = `${booking.customer}`
-        item.editable = false
-        return item
-    })
-    tasks.value = items
-    calendarOptions.value.events = items
-    getCalendarLegend();
 }
 </script>
 
