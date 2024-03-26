db361cadb5cf2b73b1f020df8b54564105232581
Daniil Bobrov <allodrmen@yandex.ru>
2024-03-26 18:22:13 +0300

    refactoring SearchForm.vue. Changed data from props to store

--- components/SearchForm.vue
+++ components/SearchForm.vue
@@ -4,59 +4,40 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 onMounted(() => {
     setFormInitialData();
 })
-const queries = useRoute().query
-const setFormInitialData = () => {
-    if (Object.keys(queries).length === 0) {
-        form.value = {
-            city: '',
-            adult: adult.value,
-            children: children.value,
-            date: {
-                start: '',
-                end: ''
-            }
-        }
-        return;
-    }
-    form.value = {
-        city: queries.city ? queries.city : '',
-        adult: queries.adult ? Number(queries.adult) : 1,
-        children: queries.children ? Number(queries.children) : 0,
-        date: {
-            start: queries.start ? queries.start : '',
-            end: queries.end ? queries.end : ''
-        }
-    }
-}
 const props = defineProps({
     formTitle: {
         type: String,
         required: true,
     },
 })
-const adult = ref(1)
-const children = ref(0)
 const isShowDropdown = ref(false)
 const isDisabledButton = ref(false);
-const { errorCityNotOnList } = getErrorsCodes();
 const errors = ref({
     errorCityNotOnList: false,
 })
-
-const handleDate = (modelData) => {
-    form.value.date.start = parseDate(modelData[0]);
-    form.value.date.end = parseDate(modelData[1]);
+const citiesList = ref([
+    'Ставрополь',
+    'Пятигорск',
+    'Кисловодск'
+])
+const queries = useRoute().query
+const searchApartments = useSearchApartmentsStore()
+const { errorCityNotOnList } = getErrorsCodes();
+const setFormInitialData = () => {
+    if (Object.keys(queries).length === 0) {
+        searchApartments.setInitialDate();
+        return;
+    }
+    searchApartments.adult = queries.adult ? Number(queries.adult) : 1
+    searchApartments.children = queries.children ? Number(queries.children) : 0
+    searchApartments.city = queries.city ? queries.city : ''
+    searchApartments.setInitialDate(queries.start, queries.end)
 }
 
-const form = ref({
-    city: '',
-    adult: 1,
-    children: 0,
-    date: {
-        start: '',
-        end: ''
-    }
-})
+const handleDate = (modelData) => {
+    searchApartments.date[0] = modelData[0];
+    searchApartments.date[1] = modelData[1];
+}
 const resetErrors = () => {
     Object.keys(errors.value).forEach((key) => errors.value[key] = false)
 }
@@ -64,28 +45,26 @@ const resetErrors = () => {
 const submitForm = async () => {
     resetErrors();
     isDisabledButton.value = true;
-    const isCityOnList = citiesList.value.some((city) => form.value.city === city)
+    const isCityOnList = citiesList.value.some((city) => searchApartments.city === city)
     if (!isCityOnList) {
         isDisabledButton.value = false;
         errors.value.errorCityNotOnList = true
         return
     }
     try {
-        useSearchApartmentsStore().adult = Number(form.value.adult);
-        useSearchApartmentsStore().children = Number(form.value.children);
         useSearchApartmentsStore().process = 'loading';
-        await useSearchApartmentsStore().getGeoDataCity(form.value.city);
-        await useSearchApartmentsStore().loadSearchApartments(form.value.date.start, form.value.date.end);
+        await useSearchApartmentsStore().getGeoDataCity(searchApartments.city);
+        await useSearchApartmentsStore().loadSearchApartments(parseDate(searchApartments.date[0]), parseDate(searchApartments.date[1]));
         await useSearchApartmentsStore().getGeoDataFromApartments();
         useSearchApartmentsStore().process = 'loaded';
         navigateTo({
             path: '/search',
             query: {
-                city: form.value.city,
-                adult: form.value.adult,
-                children: form.value.children,
-                start: form.value.date.start,
-                end: form.value.date.end
+                city: searchApartments.city,
+                adult: searchApartments.adult,
+                children: searchApartments.children,
+                start: parseDate(searchApartments.date[0]),
+                end: parseDate(searchApartments.date[1])
             }
         })
         
@@ -97,12 +76,6 @@ const submitForm = async () => {
         isDisabledButton.value = false;
     }
 }
-
-const citiesList = ref([
-    'Ставрополь',
-    'Пятигорск',
-    'Кисловодск'
-])
 </script>
 
 <template>
@@ -114,7 +87,7 @@ const citiesList = ref([
     <form @submit.prevent="submitForm" class="row g-3">
         <div class="col-md-6">
             <label for="datalist" class="form-label">Выберите город</label>
-            <input v-model="form.city" class="form-control" :class="{ 'is-invalid': errors.errorCityNotOnList }"
+            <input v-model="searchApartments.city" class="form-control" :class="{ 'is-invalid': errors.errorCityNotOnList }"
                 list="datalistOptions" id="datalist" required>
             <datalist id="datalistOptions">
                 <option v-for="(city, index) in citiesList" :key="index" :value="city" />
@@ -127,7 +100,7 @@ const citiesList = ref([
                 <button @click="isShowDropdown = !isShowDropdown" type="button" class="form-control text-end"
                     id="guests">
                     <span class="d-block w-100 text-start">
-                        {{ form.adult }} взрослых, {{ form.children }} детей
+                        {{ searchApartments.adult }} взрослых, {{ searchApartments.children }} детей
                     </span>
                     <ClientOnly>
                         <FontAwesomeIcon class="caret-down-icon position-absolute" :icon="['fas', 'caret-down']" />
@@ -143,10 +116,10 @@ const citiesList = ref([
                             <span>От 18 лет</span>
                         </div>
                         <div class="col">
-                            <button @click="--form.adult" :disabled="form.adult === 0" type="button"
+                            <button @click="--searchApartments.adult" :disabled="searchApartments.adult === 0" type="button"
                                 class="btn-counter rounded-circle border-0 fs-4 me-2">-</button>
-                            <span>{{ form.adult }}</span>
-                            <button @click="++form.adult" :disabled="form.adult === 4" type="button"
+                            <span>{{ searchApartments.adult }}</span>
+                            <button @click="++searchApartments.adult" :disabled="searchApartments.adult === 4" type="button"
                                 class="btn-counter rounded-circle border-0 fs-4 ms-2">+</button>
                         </div>
                     </div>
@@ -161,10 +134,10 @@ const citiesList = ref([
                             <span>От 0 до 17 лет</span>
                         </div>
                         <div class="col">
-                            <button @click="--form.children" :disabled="form.children === 0" type="button"
+                            <button @click="--searchApartments.children" :disabled="searchApartments.children === 0" type="button"
                                 class="btn-counter rounded-circle border-0 fs-4 me-2">-</button>
-                            <span>{{ form.children }}</span>
-                            <button @click="++form.children" :disabled="form.children === 4" type="button"
+                            <span>{{ searchApartments.children }}</span>
+                            <button @click="++searchApartments.children" :disabled="searchApartments.children === 4" type="button"
                                 class="btn-counter rounded-circle border-0 fs-4 ms-2">+</button>
                         </div>
                     </div>
@@ -183,16 +156,16 @@ const citiesList = ref([
         <div class="col-md-6">
             <label for="start" class="form-label">Заезд</label>
             <ClientOnly>
-                <VueDatePicker v-model="form.date" @update:model-value="handleDate" :auto-position="false" auto-apply
+                <VueDatePicker v-model="searchApartments.date" @update:model-value="handleDate" :auto-position="false" auto-apply
                     id="start" range text-input :enable-time-picker="false" :clearable="false" locale="ru"
                     :min-date="new Date()">
 
                     <template #dp-input>
-                        <input type="text" :value="form.date.start" class="form-control" required>
+                        <input type="text" :value="parseDate(searchApartments.date[0])" class="form-control" required>
                     </template>
 
                     <template #input-icon>
-                        <FontAwesomeIcon v-if="!form.date.start" class="datepicker-calendar-icon"
+                        <FontAwesomeIcon v-if="!searchApartments.date[0]" class="datepicker-calendar-icon"
                             :icon="['far', 'calendar']" />
                     </template>
                 </VueDatePicker>
@@ -202,16 +175,16 @@ const citiesList = ref([
         <div class="col-md-6">
             <label for="end" class="form-label">Выезд</label>
             <ClientOnly>
-                <VueDatePicker v-model="form.date" @update:model-value="handleDate" :auto-position="false" auto-apply
+                <VueDatePicker v-model="searchApartments.date" @update:model-value="handleDate" :auto-position="false" auto-apply
                     id="end" range text-input :enable-time-picker="false" locale="ru" :clearable="false"
                     :min-date="new Date()">
 
                     <template #dp-input>
-                        <input type="text" :value="form.date.end" class="form-control" required>
+                        <input type="text" :value="parseDate(searchApartments.date[1])" class="form-control" required>
                     </template>
 
                     <template #input-icon>
-                        <FontAwesomeIcon v-if="!form.date.end" class="datepicker-calendar-icon"
+                        <FontAwesomeIcon v-if="!searchApartments.date[1]" class="datepicker-calendar-icon"
                             :icon="['far', 'calendar']" />
                     </template>
                 </VueDatePicker>
