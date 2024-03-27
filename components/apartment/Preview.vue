4c1d22001d9ca7f4cd714e1de708fffbf2005c04
Daniil Bobrov <allodrmen@yandex.ru>
2024-03-27 18:03:08 +0300

    added dropdown to actions with card

--- components/apartment/Preview.vue
+++ components/apartment/Preview.vue
@@ -2,38 +2,52 @@
     <div class="card h-100">
         <div class="row">
             <div class="col-12" :class="{ 'col-md-6': isVerticalLayout }">
-                <div v-if="props.photos.length !== 0" ref="sliderContainer" :id="`sliderApartmentPreview${props.id}`" class="sliderApartmentPreview carousel slide">
+                <div v-if="props.photos.length !== 0" ref="sliderContainer" :id="`sliderApartmentPreview${props.id}`"
+                    class="sliderApartmentPreview carousel slide">
                     <div class="carousel-indicators">
-                        <button 
-                        v-for="(photo, index) in props.photos" 
-                        :key="index" 
-                        type="button" 
-                        :data-bs-target="`#sliderApartmentPreview${props.id}`"
-                        :data-bs-slide-to="index"
-                        :aria-current="index === 0"
-                        :aria-label="`Slide ${index + 1}`"
-                        :class="{ 'active': index === 0 }"
-                        >
+                        <button v-for="(photo, index) in props.photos" :key="index" type="button"
+                            :data-bs-target="`#sliderApartmentPreview${props.id}`" :data-bs-slide-to="index"
+                            :aria-current="index === 0" :aria-label="`Slide ${index + 1}`"
+                            :class="{ 'active': index === 0 }">
                         </button>
                     </div>
                     <div class="carousel-inner">
-                        <div v-for="(photo, index) in props.photos" :class="{ 'active': index === 0 }" class="carousel-item">
+                        <div v-for="(photo, index) in props.photos" :class="{ 'active': index === 0 }"
+                            class="carousel-item">
                             <NuxtImg :src="photo.thumb" :alt="`Комната ${index + 1}`" class="img-slider card-img-top"
                                 sizes="md:300px" />
                         </div>
                     </div>
                     <div class="carousel-split position-absolute w-100 h-100 top-0 d-flex">
-                        <div @mouseover="toggleSlide(index)" v-for="(photo, index) in props.photos" :key="index" class="col h-100"></div>
+                        <div @mouseover="toggleSlide(index)" v-for="(photo, index) in props.photos" :key="index"
+                            class="col h-100"></div>
                     </div>
                 </div>
-                <div v-else class="slider-no-photo d-flex align-items-center justify-content-center bg-secondary text-white">
+                <div v-else
+                    class="slider-no-photo d-flex align-items-center justify-content-center bg-secondary text-white">
                     <span class="fs-3">Фотографий нет</span>
                 </div>
             </div>
-            <div class="col-12" :class="{ 'col-md-6 d-flex align-items-center': isVerticalLayout,}">
+            <div class="col-12" :class="{ 'col-md-6 d-flex align-items-center': isVerticalLayout, }">
                 <div class="card-body">
-                    <h5 class="card-title">{{ props.name }}</h5>
-                    <!-- кол-во человек, колво-кроватей, квадратура -->
+                    <div class="d-flex justify-content-between">
+                        <h5 class="card-title">{{ props.name }}</h5>
+                        <div class="dropdown">
+                            <button type="button" class="btn btn-extend" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <ClientOnly>
+                                    <FontAwesomeIcon :icon="['fas', 'ellipsis']" />
+                                </ClientOnly>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <NuxtLink :to="`/apartments/edit/${props.id}`" class="dropdown-item">Редактировать</NuxtLink>
+                                </li>
+                                <li><a class="dropdown-item" href="#">В архив</a></li>
+                                <li><a class="dropdown-item" href="#">Удалить</a></li>
+                            </ul>
+                        </div>
+                    </div>
                     <div class="d-flex flex-wrap mb-2">
                         <div>
                             <span class="badge rounded-pill text-bg-light">{{ props.maxGuests }} гостя</span>
@@ -41,12 +55,13 @@
                         <div>
                             <span class="badge rounded-pill text-bg-light">{{ props.roomsCount }} комнаты</span>
                         </div>
-                        <!-- TODO добавить количество кроватей -->                
+                        <!-- TODO добавить количество кроватей -->
                         <!-- TODO добавить количество квадратов -->
                     </div>
                     <p class="card-text mb-2">{{ props.city }}, {{ props.address }}</p>
                     <div>
-                        <button @click="navigateToPage" type="button" class="btn btn-primary">{{props.price}}₽ за сутки</button>
+                        <button @click="navigateToPage" type="button" class="btn btn-primary">{{ props.price }}₽ за
+                            сутки</button>
                     </div>
                 </div>
             </div>
