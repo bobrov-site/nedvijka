1147e3ed94c3ed73085bd4c6e997fe7a883ee45f
Daniil Bobrov <allodrmen@yandex.ru>
2024-03-27 11:09:02 +0300

    added errorBookingNotFound and errorBookingNotAuth codes

--- utils/getErrorsCodes.js
+++ utils/getErrorsCodes.js
@@ -4,6 +4,8 @@ export default (() => {
         errorPasswordNotMatch: 'Пароль подтверждения должен совпадать',
         errorPasswordLength: 'Пароль должен быть минимум из 6 символов',
         errorPasswordWrong: 'Неправильный пароль',
-        errorCityNotOnList: 'Город не найден в списке'
+        errorCityNotOnList: 'Город не найден в списке',
+        errorBookingNotFound: 'Невозможно cделать бронь в эти даты или укажите другое количество гостей',
+        errorBookingNotAuth: 'Невозможно cделать бронь так как вы неавторизованы. Авторизуйтесь или зарегистрируйтесь на сайте'
     }
 })
