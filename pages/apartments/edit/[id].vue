d3017fb00ef842729feb81618c7b8db0cd3dd16a
Daniil Bobrov <allodrmen@yandex.ru>
2024-03-27 18:41:01 +0300

    added layout to edit page

--- pages/apartments/edit/[id].vue
+++ pages/apartments/edit/[id].vue
@@ -1,11 +1,37 @@
 <template>
-    <div>
-        hello
+    <div class="card">
+        <div class="card-body">
+            <h1 class="card-title">Заполните информацию о вашей квартире</h1>
+            <UiFormEdit :apartment="apartment"/>
+            <button class="btn btn-primary btn-lg">Создать квартиру</button>
+        </div>
     </div>
 </template>
 
 <script setup>
+onMounted(() => {
+    loadApartment();
+})
+const apartmentId = useRoute().params.id
+const apartment = ref({
+    name: '',
+    description: '',
+    images: [],
+    countRooms: '',
+    countBeds: '',
+    countSquare: '',
+    maxGuests: '',
+    city: '',
+    address: '',
+    comfortsList: [],
+    priceDay: '',
+});
 
+const loadApartment = async() => {
+    const { id } = useRoute().params
+    // const apartments = await loadApartmentsBnovo();
+    // apartment.value = apartments.find((apartment) => apartment.id == id)
+}
 </script>
 
 <style lang="scss" scoped>
